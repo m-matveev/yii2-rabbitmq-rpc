584b4ce5dfd53cce0a5ee17e311d72443c81be66
Mikhail Matveev <m.matveev114@gmail.com>
2017-07-13 18:18:48 +0300

    component ready 4 test

---
+++ src/RabbitComponent.php
@@ -0,0 +1,165 @@
+<?php
+namespace mamatveev\yii2rabbitmq;
+
+use PhpAmqpLib\Connection\AbstractConnection;
+use Thumper\RpcServer;
+use yii\base\InvalidConfigException;
+
+class RabbitComponent extends \yii\base\Component
+{
+    /**
+     * @var string
+     */
+    public $host;
+
+    /**
+     * @var int
+     */
+    public $port;
+
+    /**
+     * @var string
+     */
+    public $user;
+
+    /**
+     * @var string
+     */
+    public $password;
+
+    /**
+     * @var string connection class
+     */
+    public $amqp_connection_class = \PhpAmqpLib\Connection\AMQPStreamConnection::class;
+
+    /**
+     * @var string
+     */
+    public $vhost = '/';
+
+    /**
+     * @var bool
+     */
+    public $insist = false;
+
+    /**
+     * @var string
+     */
+    public $login_method = 'AMQPLAIN';
+
+    /**
+     * @var mixed
+     */
+    public $login_response = null;
+
+    /**
+     * @var string
+     */
+    public $locale = 'en_US';
+
+    /**
+     * @var float
+     */
+    public $connection_timeout = 3.0;
+
+    /**
+     * @var float
+     */
+    public $read_write_timeout = 3.0;
+
+    /**
+     * @var null|mixed
+     */
+    public $context = null;
+
+    /**
+     * @var bool
+     */
+    public $keepalive = false;
+
+    /**
+     * @var float
+     */
+    public $heartbeat = 0;
+
+    /**
+     * @var null|AbstractConnection
+     */
+    protected $amqp_connection = null;
+
+    /**
+     * @throws InvalidConfigException
+     */
+    public function init()
+    {
+        foreach (['host', 'user', 'port'] as $configParam)
+        {
+            if (empty($this->{$configParam})) {
+                throw new InvalidConfigException("{$configParam} cannot be empty");
+            }
+        }
+
+        if (!class_exists($this->amqp_connection_class))
+        {
+            throw new InvalidConfigException("connection class does not exist");
+        }
+
+        parent::init();
+    }
+
+    /**
+     * init a rpc client
+     * @param $exchangeName
+     * @return RpcClient
+     */
+    public function initClient($exchangeName)
+    {
+        $connection = $this->getConnection();
+        $client = new RpcClient($connection);
+        $client->initClient($exchangeName);
+        return $client;
+    }
+
+
+    /**
+     * init a rpc server
+     * @param $exchangeName
+     * @return RpcServer
+     */
+    public function initServer($exchangeName)
+    {
+        $connection = $this->getConnection();
+        $server = new RpcServer($connection);
+        $server->initServer($exchangeName);
+        return $server;
+    }
+
+
+    /**
+     * @return null|AbstractConnection
+     */
+    protected function getConnection()
+    {
+        if ($this->amqp_connection == null) {
+            $this->amqp_connection = new $this->amqp_connection_class(
+                $this->host,
+                $this->port,
+                $this->user,
+                $this->password,
+                $this->vhost,
+                $this->insist,
+                $this->login_method,
+                $this->login_response,
+                $this->locale,
+                $this->connection_timeout,
+                $this->read_write_timeout,
+                $this->context,
+                $this->keepalive,
+                $this->heartbeat
+            );
+        }
+
+        return $this->amqp_connection;
+    }
+
+}
---
+++ src/RpcClient.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace mamatveev\yii2rabbitmq;
+
+
+use PhpAmqpLib\Message\AMQPMessage;
+
+class RpcClient extends \Thumper\RpcClient
+{
+    /**
+     * @var string
+     */
+    protected $exchangeName = null;
+
+    /**
+     * count of messages sent to the queue
+     * @var int
+     */
+    protected $requestsCount = 0;
+
+    /**
+     * count of received reply's
+     * @var int
+     */
+    protected $repliesCount = 0;
+
+    /**
+     * reply's data
+     * @var array
+     */
+    protected $repliesData = [];
+
+    /**
+     * flag, did a client starts queue consume
+     * @var bool
+     */
+    protected $isConsumed = false;
+
+    /**
+     * queue response callback
+     * @var callable|null
+     */
+    protected $callback = null;
+
+    /**
+     * init a exchange client
+     * @param $exchangeName
+     */
+    public function initClient($exchangeName)
+    {
+        list($this->queueName, , ) = $this->channel->queue_declare('', false, false, true, true);
+
+        $this->exchangeName = $exchangeName;
+        $this->requestsCount = 0;
+        $this->repliesCount = 0;
+        $this->repliesData = [];
+    }
+
+    /**
+     * Send a message to exchange
+     * @param \Serializable $msgBody
+     * @param null|mixed $correlationId
+     * @param string $routingKey
+     */
+    public function addRequest($msgBody, $routingKey = '', $correlationId = null)
+    {
+        if ($correlationId === null)
+        {
+            $correlationId =  uniqid();
+        }
+
+        $msg = new AMQPMessage(serialize($msgBody),
+            [
+                'content_type' => 'text/plain',
+                'reply_to' => $this->exchangeName,
+                'correlation_id' => $correlationId
+            ]
+        );
+
+
+        $this->channel->basic_publish($msg, $this->exchangeName . '-exchange', $routingKey);
+        $this->requestsCount++;
+    }
+
+
+    /**
+     * get a reply's from rpc server
+     * if callback set reply data will be empty
+     * @param callable|null $callback a reply handler
+     * @return array
+     */
+    public function getReplies(callable $callback = null)
+    {
+        $this->consume([$this, 'processMessage'], $callback);
+        $this->waitReply();
+        return $this->repliesData;
+    }
+
+    /**
+     * wait a messages execution
+     * @return bool
+     */
+    public function waitExecution()
+    {
+        $this->consume([$this, 'ignoreMessage']);
+        $this->waitReply();
+        return true;
+    }
+
+    /**
+     * rpc server messages consumer
+     * save message data or call user specify callback
+     * @param AMQPMessage $msg
+     */
+    public function processMessage($msg)
+    {
+        $this->repliesCount++;
+        $reply = unserialize($msg->body);
+
+        if ($this->callback === null) {
+            $this->repliesData[] = $reply;
+            return;
+        }
+
+        call_user_func($this->callback, $reply);
+    }
+
+    /**
+     * rpc server messages callback
+     * ignore response data
+     * @param AMQPMessage $msg
+     */
+    public function ignoreMessage($msg)
+    {
+        $this->repliesCount++;
+    }
+
+    /**
+     * @param callable $consumeHandler
+     * @param callable|null $replyHandler
+     */
+    protected function consume(callable $consumeHandler, callable $replyHandler = null)
+    {
+        if ($this->isConsumed)
+        {
+            $this->channel->basic_cancel($this->queueName);
+        }
+
+        $this->callback = $replyHandler;
+
+        $this->channel->basic_consume(
+            $this->queueName,
+            $this->queueName,
+            false,
+            true,
+            false,
+            false,
+            $consumeHandler
+        );
+
+        $this->isConsumed = true;
+    }
+
+    /**
+     * wait a reply's from rpc server
+     */
+    protected function waitReply()
+    {
+        while ($this->repliesCount < $this->requestsCount)
+        {
+            $this->channel->wait(null, null, $this->requestTimeout);
+        }
+
+        $this->requestsCount = 0;
+        $this->repliesCount = 0;
+        $this->callback = null;
+    }
+
+
+    /**
+     * return a count of messages sent to the queue
+     * @return int
+     */
+    public function getRequestCount()
+    {
+        return $this->requestsCount;
+    }
+
+    /**
+     * return a count of received reply's
+     * @return int
+     */
+    public function getReplyCount()
+    {
+        return $this->repliesCount;
+    }
+}
